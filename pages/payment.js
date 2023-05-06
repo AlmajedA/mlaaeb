105ee4d41f1a91f199abab5acd39ab3228cb37cb
Abdulhamid Alfaqih <sahero.xx@gmail.com>
2023-05-06 13:33:03 +0300

    Centerd content + added name of card + some other changes

--- pages/payment.js
+++ pages/payment.js
@@ -4,31 +4,37 @@ import styles from "../styles/Payment.module.css";
 
 const Payment = () => {
   return (
-    <Container className={styles.container}>
-      <Row className={styles.title}>
+    <Container className="d-flex flex-column justify-content-center align-items-center">
+      <Row className="my-auto">
         <Col>
-          <h1>Payment Page</h1>
+          <h2>Payment Page</h2>
         </Col>
       </Row>
-      <Row className={styles.paymentContainer}>
-        <Col>
-          <Form className={styles.form}>
+      <Row className="my-auto">
+      <Col className="shadow-lg rounded p-5">
+          <Form className="text-center">
+
+            <Form.Group controlId="formName">
+              <Form.Label>Name on Card</Form.Label>
+              <Form.Control type="text" placeholder="Enter your name on card" className="custom-form-control" />
+            </Form.Group>
+
             <Form.Group controlId="formCardNumber">
               <Form.Label>Card Number</Form.Label>
-              <Form.Control type="text" placeholder="Enter card number" />
+              <Form.Control type="text" placeholder="Enter card number" className="custom-form-control" />
             </Form.Group>
 
             <Form.Group controlId="formExpirationDate">
               <Form.Label>Expiration Date</Form.Label>
-              <Form.Control type="text" placeholder="MM/YY" />
+              <Form.Control type="text" placeholder="MM/YY" className="custom-form-control" />
             </Form.Group>
 
             <Form.Group controlId="formCVC">
               <Form.Label>CVC</Form.Label>
-              <Form.Control type="text" placeholder="Enter CVC" />
+              <Form.Control type="text" placeholder="Enter CVC" className="custom-form-control" />
             </Form.Group>
 
-            <Button variant="primary" type="submit">
+            <Button variant="primary" type="submit" className="mt-3">
               Proceed to Checkout
             </Button>
           </Form>
