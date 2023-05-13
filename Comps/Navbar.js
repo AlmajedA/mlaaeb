178326a3637865a964c82ead7ddf8a3e66d95712
Ali Almarzooq <94115088+Ali100i@users.noreply.github.com>
2023-05-13 20:06:50 +0300

    myBookings in user navbar & myBookings.js file

--- Comps/Navbar.js
+++ Comps/Navbar.js
@@ -1,73 +1,90 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 import { useEffect, useState } from "react";
-import Dropdown from 'react-bootstrap/Dropdown';
-import { useCookies } from 'react-cookie';
-import { useRouter } from 'next/router'
-
-
-
+import Dropdown from "react-bootstrap/Dropdown";
+import { useCookies } from "react-cookie";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
-      
-  const [cookies, setCookie, removeCookie] = useCookies(['user']);
-  const [user, setUser] = useState(null)
+  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+  const [user, setUser] = useState(null);
 
-  useEffect(() => setUser(cookies.user), [])
+  useEffect(() => setUser(cookies.user), []);
   const router = useRouter();
 
+  const signOut = () => {
+    setUser(null);
+    removeCookie("user");
+    router.replace("/");
+  };
 
-    
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm border-bottom">
+      <div className="container-fluid">
+        <Link href={"/"}>
+          <Image
+            src="/LOGO.png"
+            alt=""
+            className="navbar-brand"
+            width={128}
+            height={64}
+          />
+        </Link>
 
-    const signOut = () => {
-        setUser(null)
-        removeCookie('user');
-        router.replace("/");
-    }
+        {user ? (
+          <Dropdown>
+            <Dropdown.Toggle
+              style={{
+                background: "transparent",
+                border: "none",
+                boxShadow: "none",
+              }}
+              id="dropdown-basic"
+            >
+              <Image src="/user.png" alt="" width={64} height={64} />
+            </Dropdown.Toggle>
 
+            <Dropdown.Menu>
+              <Dropdown.Item href="/profile">Profile</Dropdown.Item>
 
+              {user.type === "user" && (
+                <>
+                  <Dropdown.Item href={`/${user.id}/myBookings`}>
+                    My Bookings
+                  </Dropdown.Item>
+                </>
+              )}
+              {user.type === "owner" && (
+                <>
+                  <Dropdown.Item href={`/${user.id}/myCourts`}>
+                    My Courts
+                  </Dropdown.Item>
+                  <Dropdown.Item href={`/${user.id}/new`}>
+                    Add Court
+                  </Dropdown.Item>
+                </>
+              )}
+              <Dropdown.Item as="button" onClick={signOut}>
+                Log out
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        ) : (
+          <div className="d-flex">
+            <Link
+              className="btn btn-outline-success m-2"
+              href="/registration/login"
+            >
+              Sign in
+            </Link>
+            <Link className="btn btn-success m-2" href="/registration/signup">
+              Sign up
+            </Link>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
-    return ( 
-        <nav className='navbar navbar-expand-lg navbar-light bg-light shadow-sm border-bottom'>
-            <div className='container-fluid'>
-                <Link href={'/'}>
-                    <Image src="/LOGO.png" alt="" className='navbar-brand' width={128} height={64}/>
-                </Link>
-                    
-                {
-                    user ? (
-                        
-                        <Dropdown>
-                            <Dropdown.Toggle style={{background: 'transparent', border: 'none', boxShadow: 'none' }} id="dropdown-basic">
-                                <Image src="/user.png" alt="" width={64} height={64}/>
-                            </Dropdown.Toggle>
-
-                            <Dropdown.Menu>
-                                <Dropdown.Item href="/profile">Profile</Dropdown.Item>
-                                {user.type === 'owner' && (
-                                    <>
-                                        <Dropdown.Item href={`/${user.id}/myCourts`}>My Courts</Dropdown.Item>
-                                        <Dropdown.Item href={`/${user.id}/new`}>Add Court</Dropdown.Item>
-                                    </>
-                                )}
-                                <Dropdown.Item as="button" onClick={signOut}>Log out</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    ) : (
-                
-                
-                <div className="d-flex">
-
-
-                    <Link className='btn btn-outline-success m-2' href='/registration/login'>Sign in</Link>
-                    <Link className='btn btn-success m-2' href='/registration/signup'>Sign up</Link>
-
-                </div>
-
-                )}
-            </div>
-        </nav>
-     );
-}
- 
 export default Navbar;
---
+++ pages/[id]/myBookings.js
@@ -0,0 +1,13 @@
+import styles from "../../styles/myBookings.module.css";
+
+const myBookings = () => {
+  return (
+    <div className={styles.container}>
+      <header>
+        <h1>My Bookings</h1>
+      </header>
+    </div>
+  );
+};
+
+export default myBookings;
