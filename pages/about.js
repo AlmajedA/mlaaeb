096251c727ebefdbfd3c534c2ceef6f90db290ab
Ammar <ammaralmajed7@gmail.com>
2023-05-14 00:32:08 +0300

    last update

--- pages/about.js
+++ pages/about.js
@@ -16,15 +16,6 @@ const About = () => {
             <p>Follow us on Instagram for up-to-date infomation: <Link className = 'link-success' href={"https://instagram.com/mlaaebsa?igshid=YmMyMTA2M2Y="}>MLAAEB Instagram</Link></p>
 
         </div> 
-        <div className='text-center my-5'>
-            <h3>Member Contribution: </h3>
-                
-            <p><b>Ammar Almajed:</b> Main page, Search page, About page, Navbar, and footer.</p>
-            <p><b>Abdulhamid Alfaqih:</b> Profile page, website LOGO, and Instagram account.</p>
-            <p><b>Ali Almarzooq:</b> Dashboard page.</p>
-            <p><b>Ali Ibrahim:</b> Sign in and Sign out pages.</p>
-                
-        </div>
     </>);
 }
  
